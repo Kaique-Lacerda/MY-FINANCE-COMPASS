5eaa23420ce89f8014639e37391dd7c73b649282
Kaique Gabriel <149600790+Kaique-Lacerda@users.noreply.github.com>
2026-09-10 16:48:57 -0300

    icon

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -102,7 +102,7 @@ export const Route = createRootRouteWithContext<{ queryClient: QueryClient }>()(
           rel: "stylesheet",
           href: appCss,
         },
-        { rel: "icon", href: "/favicon.ico", type: "image/x-icon" },
+        { rel: "icon", href: "/opia-icon.png", type: "image/png" },
       ],
     }),
     shellComponent: RootShell,
